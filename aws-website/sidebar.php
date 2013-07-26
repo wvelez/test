6fc8bced85dd8ca59cdc98e6779c7739b73f7e0d
wvelez <wxvst2@gmail.com>
2013-07-26 16:47:04 -0400

    Update sidebar.php

--- aws-website/sidebar.php
+++ aws-website/sidebar.php
@@ -1,10 +1,10 @@
 <div id="lsidebar">
         <div id='cssmenu'>
         <ul>
-        <li><a href='index.php'><span>Home</span></a></li>
+        <li><a href='index.php'><span>CYN Studios</span></a></li>
         <li><a href='concept.php'><span>Concept Art</span></a></li>
   	<li><a href='game.php'><span>Play Game!</span></a></li> 
-        <li><a href='#'><span>Prelogue</span></a></li>
+        <li><a href='#'><span>About Game</span></a></li>
         <li><a href='#'><span>Video</span></a></li>
         <li><a href='#'><span>Credits</span></a></li>
         </ul>
